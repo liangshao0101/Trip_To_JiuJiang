cb19656635aed6ada79c98cefdf964f75c7d957c
赵建良 <1974854580@qq.com>
2020-10-28 10:36:29 +0800

    这是我第一次提交

---
+++ app/src/main/java/com/example/trip_to_jiujiang/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.trip_to_jiujiang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_layout);
+    }
+}
